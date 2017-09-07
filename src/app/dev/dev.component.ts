b88bd40d5c4f94d22ec2fe21f6f0efe8bfcd8b0e
Emily Davis <emilyanndavis@gmail.com>
2017-09-07 00:43:55 -0600

    Adds (text) content to dev & translation pages

--- src/app/dev/dev.component.ts
+++ src/app/dev/dev.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dev',
-  templateUrl: './dev.component.html',
-  styleUrls: ['./dev.component.scss']
+    selector: 'app-dev',
+    templateUrl: './dev.component.html',
+    styleUrls: ['./dev.component.scss']
 })
 export class DevComponent implements OnInit {
 
-  constructor() { }
+    public languages: string[] = [];
+    public libsAndFrameworks: string[] = [];
+    public educationList: Education[] = [];
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    public ngOnInit(): void {
+        this._populateLists();
+    }
+
+    private _populateLists(): void {
+        this.languages = [
+            'HTML5', 
+            'CSS3', 
+            'JavaScript', 
+            'TypeScript', 
+            'PHP', 
+            'C#', 
+            'Python', 
+            'Git'
+        ];
+        this.libsAndFrameworks = [
+            'Angular (2+)', 
+            'AngularJS (1.x)', 
+            'jQuery', 
+            'Bootstrap',
+            'Sass', 
+            'Phalcon', 
+            '.NET MVC', 
+            'Ionic', 
+            'MongoDB'
+        ];
+        this.educationList = [
+            new Education('BoiseCodeWorks', 'Full Stack Web Development', 'Sept 2016'),
+            new Education('MongoDB University', 'MongoDB for Developers (M101P)', 'Feb 2017')
+        ];
+    }
+
+}
+
+export class Education {
+    constructor( public school: string,
+                 public program: string, 
+                 public completedDate: string ) {
+        
+    }
 }
--- src/app/translation/translation.component.ts
+++ src/app/translation/translation.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-translation',
-  templateUrl: './translation.component.html',
-  styleUrls: ['./translation.component.scss']
+    selector: 'app-translation',
+    templateUrl: './translation.component.html',
+    styleUrls: ['./translation.component.scss']
 })
 export class TranslationComponent implements OnInit {
 
-  constructor() { }
+    public publications: Publication[] = [];
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    public ngOnInit(): void {
+        this._populateData();
+    }
+
+    private _populateData(): void {
+        let unamuno: Publication = new Publication(
+            'Words Without Borders', 
+            'https://www.wordswithoutborders.org/article/the-man-who-buried-himself', 
+            'Read More'
+        );
+        unamuno.pieces = [
+            new PublicationPiece('"The Man Who Buried Himself"', 'Miguel de Unamuno')
+        ];
+        let forests: Publication = new Publication(
+            'Open Letter Books',
+            'https://www.openletterbooks.org/collections/books/products/a-thousand-forests-in-one-acorn',
+            'Read More',
+            'A Thousand Forests in One Acorn: An Anthology of Spanish-Language Fiction'
+        );
+        forests.pieces = [
+            new PublicationPiece('The Blind Ants', 'Ramiro Pinilla', true),
+            new PublicationPiece('"The Journey"', 'Cristina Fernández Cubas'),
+            new PublicationPiece('"The Angle of Horror"', 'Cristina Fernández Cubas'),
+            new PublicationPiece('The Year of Grace', 'Cristina Fernández Cubas', true),
+            new PublicationPiece('Crematorium', 'Rafael Chirbes', true)            
+        ];
+        let bogota: Publication = new Publication(
+            'Oneworld Publications',
+            '#',
+            '',
+            'Bogotá 39: New Voices from Latin America'
+        );
+        bogota.pieces = [
+            new PublicationPiece('Short Story TBD (Forthcoming in 2018)', 'Jesús Miguel Soto')
+        ];
+        this.publications = [
+            unamuno, 
+            forests,
+            bogota
+        ];
+    }
+
+}
+
+export class Publication {
+
+    public pieces: PublicationPiece[] = [];
+
+    constructor( public publisher: string,
+                 public linkHref: string,
+                 public linkText: string,
+                 public collection: string = '' ) {
+
+    }
+}
+
+export class PublicationPiece {
+    constructor( public title: string, 
+                 public author: string,
+                 public excerpt: boolean = false ) {
+
+    }
 }
