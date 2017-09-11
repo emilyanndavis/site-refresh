3a9fbc7c238777c362fc795cddff2e63c0ea1bde
Emily Davis <emilyanndavis@gmail.com>
2017-09-10 23:45:25 -0600

    Translation page styling adjustments. Also hides forthcoming publication for now.

--- src/app/translation/translation.component.ts
+++ src/app/translation/translation.component.ts
@@ -16,61 +16,84 @@ export class TranslationComponent implements OnInit {
     }
 
     private _populateData(): void {
-        let unamuno: Publication = new Publication(
+        let wordsWithoutBorders: Publication = new Publication(
             'Words Without Borders', 
             'https://www.wordswithoutborders.org/article/the-man-who-buried-himself', 
             'Read More'
         );
+        let unamuno: PublicationAuthor = new PublicationAuthor( 'Miguel de Unamuno' );
         unamuno.pieces = [
-            new PublicationPiece('"The Man Who Buried Himself"', 'Miguel de Unamuno')
+            new PublicationPiece('"The Man Who Buried Himself"')
         ];
-        let forests: Publication = new Publication(
+        wordsWithoutBorders.authors = [ unamuno ];
+
+        let aThousandForests: Publication = new Publication(
             'Open Letter Books',
             'https://www.openletterbooks.org/collections/books/products/a-thousand-forests-in-one-acorn',
             'Read More',
             'A Thousand Forests in One Acorn: An Anthology of Spanish-Language Fiction'
         );
-        forests.pieces = [
-            new PublicationPiece('The Blind Ants', 'Ramiro Pinilla', true),
-            new PublicationPiece('"The Journey"', 'Cristina Fernández Cubas'),
-            new PublicationPiece('"The Angle of Horror"', 'Cristina Fernández Cubas'),
-            new PublicationPiece('The Year of Grace', 'Cristina Fernández Cubas', true),
-            new PublicationPiece('Crematorium', 'Rafael Chirbes', true)            
+        let chirbes = new PublicationAuthor('Rafael Chirbes');
+        chirbes.pieces = [
+            new PublicationPiece('Crematorium', true)
         ];
-        let bogota: Publication = new Publication(
+        let fernandezCubas = new PublicationAuthor('Cristina Fernández Cubas');
+        fernandezCubas.pieces = [
+            new PublicationPiece('"The Journey"'),
+            new PublicationPiece('"The Angle of Horror"'),
+            new PublicationPiece('The Year of Grace', true)
+        ];
+        let pinilla = new PublicationAuthor('Ramiro Pinilla');
+        pinilla.pieces = [
+            new PublicationPiece('The Blind Ants', true)            
+        ];
+        aThousandForests.authors = [ chirbes, fernandezCubas, pinilla ];
+
+        let bogota39: Publication = new Publication(
             'Oneworld Publications',
             '#',
             '',
             'Bogotá 39: New Voices from Latin America'
         );
-        bogota.pieces = [
-            new PublicationPiece('Short Story TBD (Forthcoming in 2018)', 'Jesús Miguel Soto')
+        let soto: PublicationAuthor = new PublicationAuthor('Jesús Miguel Soto');
+        soto.pieces = [
+            new PublicationPiece('Short Story TBD (Forthcoming in 2018)')
         ];
+        bogota39.authors = [ soto ];
         this.publications = [
-            unamuno, 
-            forests,
-            bogota
+            wordsWithoutBorders, 
+            aThousandForests,
+            // bogota39
         ];
+
     }
 
 }
 
 export class Publication {
 
-    public pieces: PublicationPiece[] = [];
-
+    public authors: PublicationAuthor[] = [];
+    
     constructor( public publisher: string,
                  public linkHref: string,
                  public linkText: string,
                  public collection: string = '' ) {
-
+            
     }
 }
 
-export class PublicationPiece {
-    constructor( public title: string, 
-                 public author: string,
-                 public excerpt: boolean = false ) {
+export class PublicationAuthor {
 
+    public pieces: PublicationPiece[] = [];
+
+    constructor( public name: string ) {
+
+    }
+}
+    
+export class PublicationPiece {
+    constructor( public title: string,
+                 public excerpt: boolean = false ) {
+            
     }
 }
